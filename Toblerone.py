bd9f502fc53a36cbcff705252934d27fcca82744
Aidan Patrick Marias <52266188+Silver0006@users.noreply.github.com>
2023-02-06 21:32:30 -0500

    Add files via upload

--- Toblerone.py
+++ Toblerone.py
@@ -1,5 +1,6 @@
 
 import tkinter
+from playsound import playsound
 from tkinter import *
 from PIL import Image, ImageTk
 root = Tk()
@@ -7,6 +8,6 @@ root.attributes('-fullscreen',True)
 image1 = Image.open("E:\Toblerone\spooks.png")
 test = ImageTk.PhotoImage(image1)
 label1 = tkinter.Label(image=test)
-label1.resize((599, 599), Image.ANTIALIAS)
-label1.place(x=40, y=0)
+label1.place(x=0, y=0)
+playsound('E:\Toblerone\spook1.wav')
 root.mainloop()
