d278eff98a46dfac1a209e9fce571c319be30d16
Aidan Patrick Marias <52266188+Silver0006@users.noreply.github.com>
2023-02-08 14:59:34 -0500

    Add files via upload

--- Toblerone.py
+++ Toblerone.py
@@ -1,12 +1,13 @@
-import tkinter
+#Potential to import audio and image into exe research later
+import tkinter, os, sys
 from playsound import playsound
 from tkinter import *
 from PIL import Image, ImageTk
 root = Tk()
 root.attributes('-fullscreen',True)
-image1 = Image.open("E:\Toblerone\spooks.png")
+image1 = Image.open(os.path.join(sys.path[0], "spooks.png"), "r")
 test = ImageTk.PhotoImage(image1)
 label1 = tkinter.Label(image=test)
 label1.place(x=0, y=0)
-playsound('E:\Toblerone\spook1.wav', block=False)
+playsound(os.path.join(sys.path[0], "spook1.wav"), block=False)
 root.mainloop()
