b85704374db6fa8b0133cb9f1483135182248619
Aidan Patrick Marias <52266188+Silver0006@users.noreply.github.com>
2023-02-15 19:49:08 -0500

    Add files via upload

--- Toblerone.py
+++ Toblerone.py
@@ -1,6 +1,5 @@
 import random, time, os, subprocess, sys
-os.system(r'SchTasks /Create /SC DAILY /TN "My Task" /TR "C:mytask.bat" /ST 09:00')
-list1 = [30, 45, 60, 360, 600, 21, 49, 63, 451]
+list1 = [30, 45, 60, 360, 120, 21, 49, 63, 246]
 print(random.choice(list1))
 time.sleep(random.choice(list1))
 import Toblerone2
