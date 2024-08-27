22bb9c6dada2150f7c8c2ad51c10be0a9668fa2e
Silver0006 <52266188+Silver0006@users.noreply.github.com>
2024-08-27 12:39:39 -0400

    Add files via upload

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,15 +1,76 @@
+#define MINIAUDIO_IMPLEMENTATION
 #include <iostream>
-#include <vector>
 #include <random>
-#include <SFML/Audio.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include <toblerone/spooks_png.h>
-#include <toblerone/spook1_ogg.h>
-#include <toblerone/spook2_ogg.h>
-using namespace std;
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <windows.h>
+#include <miniaudio.h>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/fl_draw.H>
+#include <spook1_wav.h>
+#include <spook2_wav.h>
+#include <image_rgba.h>
+using std::vector, std::random_device , std::minstd_rand, std::uniform_int_distribution, std::cout, std::endl, std::cerr;
 
-int main() {
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount) {
+    ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
+    if (pDecoder == nullptr) {
+        return;
+    }
+
+    ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount, nullptr);
+    (void)pInput;
+}
+
+int playsound(unsigned char* file, unsigned int filel, int sleeptime) {
+    ma_result result;
+    ma_decoder decoder;
+    ma_device_config deviceConfig;
+    ma_device device;
+    result = ma_decoder_init_memory(file, filel, nullptr, &decoder);
+    if (result != MA_SUCCESS) {
+        printf("Failed to initialize decoder.\n");
+        return -1;
+    }
+
+    // Configure the playback device
+    deviceConfig = ma_device_config_init(ma_device_type_playback);
+    deviceConfig.playback.format = decoder.outputFormat;
+    deviceConfig.playback.channels = decoder.outputChannels;
+    deviceConfig.sampleRate = decoder.outputSampleRate;
+    deviceConfig.dataCallback = data_callback;
+    deviceConfig.pUserData = &decoder;
+
+    // Initialize the playback device
+    result = ma_device_init(nullptr, &deviceConfig, &device);
+    if (result != MA_SUCCESS) {
+        printf("Failed to initialize playback device.\n");
+        ma_decoder_uninit(&decoder);
+        return -1;
+    }
+    result = ma_device_start(&device);
+    if (result != MA_SUCCESS) {
+        printf("Failed to start playback device.\n");
+        ma_device_uninit(&device);
+        ma_decoder_uninit(&decoder);
+        return -1;
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(sleeptime));
+    ma_device_uninit(&device);
+    ma_decoder_uninit(&decoder);
+    return 0;
+}
+
+void play_second_sound(Fl_Window* window) {
+    playsound(spook2_wav, spook2_wav_len, 4);
+    Fl::delete_widget(window); // End the FLTK event loop after playing the sound
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
     // List of times that are selected by RNG
     vector<int> times = {15, 30, 45, 60, 360, 120, 21 ,49 ,63, 246};
     random_device rd;
@@ -19,34 +80,21 @@ int main() {
     int time = distr(gen);
     cout << "Rando says " << time+1 << endl;
     cout << "This is sleep time: " << times[time] << endl;
-    sf::sleep(sf::seconds(static_cast<float>(times[time])));
-    sf::SoundBuffer buffer;
-    if (!buffer.loadFromMemory(spook1_ogg, spook1_ogg_len)) {
-        cerr << "Error loading sound from memory" << endl;
-        return -1;
-    }
-    sf::Sound sound;
-    sound.setBuffer(buffer);
-    sound.play();
-    sf::sleep(sf::seconds(5));
-    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Spooks", sf::Style::Fullscreen);
-    sf::Texture texture;
-    if (!texture.loadFromMemory(spooks_png, spooks_png_len)) {
-        cerr << "Error loading image from memory" << endl;
-        return -1;
-    }
-    sf::Sprite sprite;
-    sprite.setTexture(texture);
-    window.setMouseCursorVisible(false);
-    window.draw(sprite);
-    window.display();
-    if (!buffer.loadFromMemory(spook2_ogg, spook2_ogg_len)) {
-        cerr << "Error loading sound from memory" << endl;
-        return -1;
-    }
-    sound.setBuffer(buffer);
-    sound.play();
-    sf::sleep(sf::seconds(4));
-    window.close();
-    main();
+    std::this_thread::sleep_for(std::chrono::seconds(times[time]));
+
+    playsound(spook1_wav, spook1_wav_len, 5);
+
+    Fl_Window *window = new Fl_Window(Fl::w(), Fl::h());
+    window->fullscreen();
+    window->color(FL_BLACK);
+    Fl_Box *box = new Fl_Box(0, 0, Fl::w(), Fl::h());
+    Fl_RGB_Image *img = new Fl_RGB_Image(image_rgba, 1080, 1080, 4);
+    box->image(img);
+    window->end();
+    window->show();
+    Fl::add_timeout(0, [](void* data) {
+        play_second_sound(static_cast<Fl_Window*>(data));
+    }, window);
+    Fl::run();
+    WinMain(hInstance, hPrevInstance, lpCmdLine, nShowCmd);
 }
